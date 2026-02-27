2b7c969b6e04d8800a5df0be6c5ea221d65ee1bb
talinemerheb <talinemerheb260@gmail.com>
2026-02-27 17:11:50 +0200

    TALINEEE

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
   return (
     <>
       <h1>Vite + React</h1>
-      <div className="card">
+      <div className="CARD345">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
