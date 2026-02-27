e3204f13e68eba3a53fffdf36b00bfe74ef65e6b
elissa <elissa.issa@bamptee.com>
2026-02-27 17:22:56 +0200

    COMMIT

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
   return (
     <>
       <h1>Vite + React</h1>
-      <div className="CARD345">
+      <div className="CARD3450">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
